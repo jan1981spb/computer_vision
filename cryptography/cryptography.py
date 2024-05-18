b00e5c6eee12e48b49a700d2593cff18af2c876b
Вячеславович Гришин Кирилл <k.grishin@voskhod.ru>
2024-05-18 21:57:39 +0300

    decrypt logic

--- cryptography/cryptography.py
+++ cryptography/cryptography.py
@@ -2,28 +2,33 @@ import random
 
 import cv2
 import numpy as np
+import os
 
 def encryptImage(img):
-    # Prepare image
+    # Конвертация в оттенки серого
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Конвертация в черно-белое представление
     im_bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 4)
     arr = np.array(im_bw, np.uint8)
+    os.remove(os.environ.get(PATH) + os.pathsep  +  'original_bw.jpg')
+    cv2.imwrite(os.environ.get(PATH) + os.pathsep  +  'original_bw.jpg', arr)
+
 
     black_ciphers = [
-        ([1, 0, 1, 0], [0, 1, 0, 1]),
-        ([1, 0, 0, 1], [0, 1, 1, 0]),
-        ([1, 1, 0, 0], [0, 0, 1, 1]),
-        ([0, 0, 1, 1], [1, 1, 0, 0]),
-        ([0, 1, 1, 0], [1, 0, 0, 1]),
-        ([0, 1, 0, 1], [1, 0, 1, 0])
+        (np.array([1, 0, 1, 0]), np.array([0, 1, 0, 1])),
+        (np.array([1, 0, 0, 1]), np.array([0, 1, 1, 0])),
+        (np.array([1, 1, 0, 0]), np.array([0, 0, 1, 1])),
+        (np.array([0, 0, 1, 1]), np.array([1, 1, 0, 0])),
+        (np.array([0, 1, 1, 0]), np.array([1, 0, 0, 1])),
+        (np.array([0, 1, 0, 1]), np.array([1, 0, 1, 0]))
     ]
     white_ciphers = [
-        ([1, 0, 1, 0], [1, 0, 1, 0]),
-        ([1, 0, 0, 1], [1, 0, 0, 1]),
-        ([1, 1, 0, 0], [1, 1, 0, 0]),
-        ([0, 0, 1, 1], [0, 0, 1, 1]),
-        ([0, 1, 1, 0], [0, 1, 1, 0]),
-        ([0, 1, 0, 1], [0, 1, 0, 1])
+        (np.array([1, 0, 1, 0]), np.array([1, 0, 1, 0])),
+        (np.array([1, 0, 0, 1]), np.array([1, 0, 0, 1])),
+        (np.array([1, 1, 0, 0]), np.array([1, 1, 0, 0])),
+        (np.array([0, 0, 1, 1]), np.array([0, 0, 1, 1])),
+        (np.array([0, 1, 1, 0]), np.array([0, 1, 1, 0])),
+        (np.array([0, 1, 0, 1]), np.array([0, 1, 0, 1]))
     ]
 
     def encrypt(pixel):
@@ -35,14 +40,10 @@ def encryptImage(img):
     privateEncrypted = {}
     publicEncrypted = {}
 
-    rows, cols = arr.shape
-    for i in range(rows):
-        for j in range(cols):
-            pixel = arr[i, j]
-          #  print(i, j, pixel)
+    for (x,y), pixel in np.ndenumerate(arr):
             encr_private, encr_public = encrypt(pixel)
-            privateEncrypted[(i, j)] = encr_private
-            publicEncrypted[(i, j)] = encr_public
+            privateEncrypted[(x, y)] = encr_private
+            publicEncrypted[(x, y)] = encr_public
 
     return (privateEncrypted, publicEncrypted)
 
@@ -50,26 +51,39 @@ def encryptImage(img):
 
 def decryptImage(privateEncrypted: dict, publicEncrypted: dict, shape: tuple):
 
-    def identify_pixel_type(encoded_pixel_in_public: list, encoded_pixel_in_private: list):
-        value = 0;
-        for el_public in encoded_pixel_in_public:
-            for el_private in encoded_pixel_in_private:
-                value = value + el_private + el_public
+    def identify_pixel_type(encoded_pixel_in_public, encoded_pixel_in_private):
+        value = encoded_pixel_in_public.dot(encoded_pixel_in_private)
         if (value == 0):
             return 255
         else:
             return 0
 
-    intermediate_arr = []
-    output_arr = np.empty((shape), dtype = int)
+    acc = []
+    buffer = []
+    cols = shape[1]
 
     for coordinate, encoded_pixel_in_private in privateEncrypted.items():
         encoded_pixel_in_public = publicEncrypted[coordinate]
         pixel = identify_pixel_type(encoded_pixel_in_public, encoded_pixel_in_private)
-        intermediate_arr.append((coordinate, pixel))
+
+        x,y = coordinate
+        if y == cols - 1:
+            buffer.append(pixel)
+            acc.append(buffer)
+            buffer = []
+        else:
+            buffer.append(pixel)
+
+    return np.array(acc)
 
 
+img = cv2.imread(os.environ.get(PATH) + os.pathsep  +  'original.jpg')
 
-img = cv2.imread('/home/djikey/IdeaProjects/computer_vision/cat.jpg')
 privateEncrypted, publicEncrypted = encryptImage(img)
-decryptImage(privateEncrypted, publicEncrypted, img.shape)
+decrypted_as_np_array = decryptImage(privateEncrypted, publicEncrypted, img.shape)
+
+os.remove(os.environ.get(PATH) + os.pathsep  +  'encoded_bw.jpg')
+cv2.imwrite(os.environ.get(PATH) + os.pathsep  +  'encoded_bw.jpg', decrypted_as_np_array)
+
+
+
