a5dfda19aaa379863fb639ce4b2b43afee544425
Вячеславович Гришин Кирилл <k.grishin@voskhod.ru>
2024-05-30 14:11:38 +0300

    decrypt logic

--- cryptography/cryptography.py
+++ cryptography/cryptography.py
@@ -4,31 +4,12 @@ import cv2
 import numpy as np
 import os
 
-def saveShares(encrypted: dict, shape: tuple, dictFileName:str):
-    rows, cols, _ = shape
-    acc = []
-    buffer1 = []
-    buffer2 = []
-    for coordinate, encoded_pixel_in_private in encrypted.items():
 
-        x,y = coordinate
-        if y == cols - 1:
-            buffer1.append(encoded_pixel_in_private[0]*255)
-            buffer1.append(encoded_pixel_in_private[1]*255)
-            buffer2.append(encoded_pixel_in_private[2]*255)
-            buffer2.append(encoded_pixel_in_private[3]*255)
-            acc.append(buffer1)
-            acc.append(buffer2)
-            buffer1 = []
-            buffer2 = []
-        else:
-            buffer1.append(encoded_pixel_in_private[0]*255)
-            buffer1.append(encoded_pixel_in_private[1]*255)
-            buffer2.append(encoded_pixel_in_private[2]*255)
-            buffer2.append(encoded_pixel_in_private[3]*255)
-    npArrayImage = np.array(acc)
-    os.remove('/home/djikey/IdeaProjects/computer_vision/' + dictFileName)
-    cv2.imwrite('/home/djikey/IdeaProjects/computer_vision/' + dictFileName, npArrayImage)
+def saveShares(privateEncrypted: np.ndarray, publicEncrypted: np.ndarray):
+  #  os.remove('/home/djikey/IdeaProjects/computer_vision/' + dictFileName)
+    cv2.imwrite('/home/djikey/IdeaProjects/computer_vision/private_encrypted.jpg', privateEncrypted)
+    cv2.imwrite('/home/djikey/IdeaProjects/computer_vision/public_encrypted.jpg', publicEncrypted)
+
 
 
 def encryptImage(img):
@@ -37,25 +18,24 @@ def encryptImage(img):
     # Конвертация в черно-белое представление
     im_bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 4)
     arr = np.array(im_bw, np.uint8)
-    os.remove('/home/djikey/IdeaProjects/computer_vision/original_bw.jpg')
+#    os.remove('/home/djikey/IdeaProjects/computer_vision/original_bw.jpg')
     cv2.imwrite('/home/djikey/IdeaProjects/computer_vision/original_bw.jpg', arr)
 
-
     black_ciphers = [
-        (np.array([1, 0, 1, 0]), np.array([0, 1, 0, 1])),
-        (np.array([1, 0, 0, 1]), np.array([0, 1, 1, 0])),
-        (np.array([1, 1, 0, 0]), np.array([0, 0, 1, 1])),
-        (np.array([0, 0, 1, 1]), np.array([1, 1, 0, 0])),
-        (np.array([0, 1, 1, 0]), np.array([1, 0, 0, 1])),
-        (np.array([0, 1, 0, 1]), np.array([1, 0, 1, 0]))
+        ([255, 0, 255, 0], [0, 255, 0, 255]),
+        ([255, 0, 0, 255], [0, 255, 255, 0]),
+        ([255, 255, 0, 0], [0, 0, 255, 255]),
+        ([0, 0, 255, 255], [255, 255, 0, 0]),
+        ([0, 255, 255, 0], [255, 0, 0, 255]),
+        ([0, 255, 0, 255], [255, 0, 255, 0])
     ]
     white_ciphers = [
-        (np.array([1, 0, 1, 0]), np.array([1, 0, 1, 0])),
-        (np.array([1, 0, 0, 1]), np.array([1, 0, 0, 1])),
-        (np.array([1, 1, 0, 0]), np.array([1, 1, 0, 0])),
-        (np.array([0, 0, 1, 1]), np.array([0, 0, 1, 1])),
-        (np.array([0, 1, 1, 0]), np.array([0, 1, 1, 0])),
-        (np.array([0, 1, 0, 1]), np.array([0, 1, 0, 1]))
+        ([255, 0, 255, 0], [255, 0, 255, 0]),
+        ([255, 0, 0, 255], [255, 0, 0, 255]),
+        ([255, 255, 0, 0], [255, 255, 0, 0]),
+        ([0, 0, 255, 255], [0, 0, 255, 255]),
+        ([0, 255, 255, 0], [0, 255, 255, 0]),
+        ([0, 255, 0, 255], [0, 255, 0, 255])
     ]
 
     def encrypt(pixel):
@@ -64,56 +44,76 @@ def encryptImage(img):
         else:
             return white_ciphers[random.randint(0, len(white_ciphers) - 1)]
 
-    privateEncrypted = {}
-    publicEncrypted = {}
+    rows, cols, _ = img.shape
+    privateEncrypted = []
+    publicEncrypted = []
+    privateEncryptedBuffer = []
+    publicEncryptedBuffer = []
 
-    for (x,y), pixel in np.ndenumerate(arr):
-            encr_private, encr_public = encrypt(pixel)
-            privateEncrypted[(x, y)] = encr_private
-            publicEncrypted[(x, y)] = encr_public
-
-    saveShares(privateEncrypted, img.shape, 'private_shares.jpg')
-    saveShares(publicEncrypted, img.shape, 'public_shares.jpg')
-
-    return (privateEncrypted, publicEncrypted)
-
-
-
-def decryptImage(privateEncrypted: dict, publicEncrypted: dict, shape: tuple):
-
-    def identify_pixel_type(encoded_pixel_in_public, encoded_pixel_in_private):
-        value = encoded_pixel_in_public.dot(encoded_pixel_in_private)
-        if (value == 0):
-            return 255
+    for (x, y), pixel in np.ndenumerate(arr):
+        encr_private, encr_public = encrypt(pixel)
+        if y == cols - 1:
+            privateEncryptedBuffer = privateEncryptedBuffer + encr_private
+            publicEncryptedBuffer = publicEncryptedBuffer + encr_public
+            privateEncrypted.append(privateEncryptedBuffer)
+            publicEncrypted.append(publicEncryptedBuffer)
+            privateEncryptedBuffer = []
+            publicEncryptedBuffer = []
         else:
-            return 0
+            privateEncryptedBuffer = privateEncryptedBuffer + encr_private
+            publicEncryptedBuffer = publicEncryptedBuffer + encr_public
+
+    return (np.array(privateEncrypted), np.array(publicEncrypted))
+
+
+def identify_pixel_type(encoded_pixel_in_public, encoded_pixel_in_private):
+    value = encoded_pixel_in_public.dot(encoded_pixel_in_private)
+    if (value == 0):
+        return 255
+    else:
+        return 0
+
+
+def decryptImage(privateEncrypted: np.ndarray, publicEncrypted: np.ndarray):
+
+    saveShares(privateEncrypted, publicEncrypted)
 
     acc = []
-    buffer = []
-    cols = shape[1]
+    rowBuffer = []
+    for rowIndex, encodedPr in enumerate(privateEncrypted, 0):
+        encodedPub = publicEncrypted[rowIndex]
+        index = 0
+        accumulatedIndex = 0
+        privBuffer = []
+        pubBuffer = []
+        while (index < len(encodedPr)):
+            privPart = encodedPr[index]
+            pubPart = encodedPub[index]
+            index = index + 1
+            if (accumulatedIndex == 3):
+                privBuffer.append(privPart)
+                privChiperNp = np.array(privBuffer)
+                pubBuffer.append(pubPart)
+                pubChiperNp = np.array(pubBuffer)
+                pixel = identify_pixel_type(privChiperNp, pubChiperNp)
+                rowBuffer.append(pixel)
+                privBuffer = []
+                pubBuffer = []
+                accumulatedIndex = 0
+            else:
+                privBuffer.append(privPart)
+                pubBuffer.append(pubPart)
+                accumulatedIndex = accumulatedIndex + 1
 
-    for coordinate, encoded_pixel_in_private in privateEncrypted.items():
-        encoded_pixel_in_public = publicEncrypted[coordinate]
-        pixel = identify_pixel_type(encoded_pixel_in_public, encoded_pixel_in_private)
-
-        x,y = coordinate
-        if y == cols - 1:
-            buffer.append(pixel)
-            acc.append(buffer)
-            buffer = []
-        else:
-            buffer.append(pixel)
+        acc.append(rowBuffer)
+        rowBuffer = []
 
     return np.array(acc)
-
 
 img = cv2.imread('/home/djikey/IdeaProjects/computer_vision/original.jpg')
 
 privateEncrypted, publicEncrypted = encryptImage(img)
-decrypted_as_np_array = decryptImage(privateEncrypted, publicEncrypted, img.shape)
+decrypted_as_np_array = decryptImage(privateEncrypted, publicEncrypted)
 
-os.remove('/home/djikey/IdeaProjects/computer_vision/encoded_bw.jpg')
+# os.remove('/home/djikey/IdeaProjects/computer_vision/encoded_bw.jpg')
 cv2.imwrite('/home/djikey/IdeaProjects/computer_vision/encoded_bw.jpg', decrypted_as_np_array)
-
-
-
